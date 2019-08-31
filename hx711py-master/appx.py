933335200750b6e2e29cdccd79f5588d70bcc1fc
algebric <p.bilas93@gmail.com>
2019-08-31 22:04:23 +0200

    Update appx.py

--- hx711py-master/appx.py
+++ hx711py-master/appx.py
@@ -8,14 +8,11 @@ from hx711 import HX711
 from numpy import median
 
 class Line :
-
 	def __init__(self,id,quan,weight,quan_max):
 		self.id = id
 		self.weight = weight
 		self.quan = quan
 		self.quan_max=quan_max
-
-
 
 def cleanAndExit():
 	print "Cleaning..."
@@ -23,37 +20,24 @@ def cleanAndExit():
 	print "Bye!"
 	sys.exit()
 
-
 contents = urllib2.urlopen("http://intshelf.azurewebsites.net/api/info").read()
 contents= contents.translate(None, '"')
 values = contents.split('},')
 
-
 for i,itr in enumerate(values):
-
 	print(itr)
-	
-
-	
 	val=itr.split(',')
-	
 	vals=[]
-
-	
 	for index,v in enumerate(val):
 		vals.append(v.split(":")[1])
-	
-	
+
 	lines=[]
-	
 	for idx in range(6):
 		lines.append(Line(vals[1],vals[2],vals[3],vals[4]))
 
 	for idx in range(6):
 		lines[idx]=(Line(vals[1],vals[2],vals[3],vals[4]))
 	
-
-
 for obj in lines:
 	print(obj.id)
 	print(obj.weight)
